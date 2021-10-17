7fa5f401f0e179d522da2091b539819682413957
imprantu <rakinhasanbabu4@gmail.com>
2021-10-17 23:56:03 +0600

    initial commit

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBzwGzTd3TVc2sqkRgKCazv_AKxX0BhcQ0",
+    authDomain: "revive-plus.firebaseapp.com",
+    projectId: "revive-plus",
+    storageBucket: "revive-plus.appspot.com",
+    messagingSenderId: "860634020793",
+    appId: "1:860634020793:web:5c5ae078daafece9079468"
+};
+
+export default firebaseConfig;
