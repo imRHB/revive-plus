dd39dc346f31f94dad4e85e41837e2434182c62e
imprantu <rakinhasanbabu4@gmail.com>
2021-10-19 18:31:54 +0600

    product details added

--- src/Pages/Details/DoctorDetails/DoctorDetails.js
+++ src/Pages/Details/DoctorDetails/DoctorDetails.js
@@ -1,34 +1,34 @@
 import React, { useEffect, useState } from 'react';
-import { Card, Row, Col } from 'react-bootstrap';
+import { Row, Col } from 'react-bootstrap';
 import { useParams } from 'react-router';
 import './DoctorDetails.css';
 
 const DoctorDetails = () => {
     const { docId } = useParams();
-    const [doctorsDetails, setDoctorsDetails] = useState([]);
-    const [docDetails, setDocDetails] = useState({});
+    const [docDetails, setDocDetails] = useState([]);
+    const [details, setDetails] = useState({});
 
     useEffect(() => {
         fetch('https://raw.githubusercontent.com/imprantu/revive-plus/main/doctors.json')
             .then(res => res.json())
-            .then(data => setDoctorsDetails(data));
+            .then(data => setDocDetails(data));
     }, []);
 
     useEffect(() => {
-        const doctorInfo = doctorsDetails.find(docDetail => docDetail?.id == docId);
-        setDocDetails(doctorInfo);
-    }, [doctorsDetails]);
+        const doctorInfo = docDetails.find(docDetail => docDetail?.id == docId);
+        setDetails(doctorInfo);
+    }, [docDetails]);
 
     return (
         <div className="container my-5">
             <div className="doc-info-card mx-auto">
                 <div className="doctor-info">
-                    <img src={docDetails?.imgWide} alt="" />
+                    <img src={details?.imgWide} alt="" />
                     <div className="text-center my-3">
-                        <h1 className="fw-bold text-primary">{docDetails?.name}</h1>
-                        <p className="fs-3 fw-bold"><span className="text-secondary">{docDetails?.role}</span> of {docDetails?.medical}</p>
-                        <p className="fs-4 fw-bold">{docDetails?.country}</p>
-                        <p className="text-muted">{docDetails?.experience} Years of Experience</p>
+                        <h1 className="fw-bold text-primary">{details?.name}</h1>
+                        <p className="fs-3 fw-bold"><span className="text-secondary">{details?.role}</span> of {details?.medical}</p>
+                        <p className="fs-4 fw-bold">{details?.country}</p>
+                        <p className="text-muted">{details?.experience} Years of Experience</p>
                     </div>
                     <hr />
                     <div>
--- src/Pages/Details/ProductDetails/ProductDetails.js
+++ src/Pages/Details/ProductDetails/ProductDetails.js
@@ -1,10 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router';
+import './ProductDetails.css';
 
 const ProductDetails = () => {
+    const { pdId } = useParams();
+    const [prodDetails, SetProdDetails] = useState([]);
+    const [details, setDetails] = useState({});
+
+    useEffect(() => {
+        fetch('https://raw.githubusercontent.com/imprantu/revive-plus/main/products.json')
+            .then(res => res.json())
+            .then(data => SetProdDetails(data));
+    }, []);
+
+    useEffect(() => {
+        const productInfo = prodDetails.find(prod => prod?.id == pdId);
+        setDetails(productInfo);
+    }, [prodDetails]);
 
     return (
         <div className="container my-5">
-            <h3>Product details of </h3>
+            <div className="d-flex align-items-center justify-content-center">
+                <div className="prod-img">
+                    <img src={details?.image} alt="" />
+                </div>
+                <div className="ms-3">
+                    <h3>{details?.name}</h3>
+                    <p className="fs-4 fw-bold">${details?.price}</p>
+                    <button className="btn btn-secondary">Buy Now</button>
+                </div>
+            </div>
         </div>
     );
 };
