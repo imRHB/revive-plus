350d4eee6320cc3de164aff2bda0de6cdf310840
imprantu <rakinhasanbabu4@gmail.com>
2021-10-19 20:44:19 +0600

    add new background feature

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ function App() {
             <Route exact path="/">
               <Home></Home>
             </Route>
-            <Route exact path="/home">
+            <Route path="/home">
               <Home></Home>
             </Route>
             <Route exact path="/services">
--- src/Pages/Details/DoctorDetails/DoctorDetails.js
+++ src/Pages/Details/DoctorDetails/DoctorDetails.js
@@ -8,6 +8,17 @@ const DoctorDetails = () => {
     const [docDetails, setDocDetails] = useState([]);
     const [details, setDetails] = useState({});
 
+    const coverImg = `${details?.imgWide}`;
+    console.log(coverImg);
+
+    const coverStyle = {
+        height: '50vh',
+        backgroundImage: 'url(' + { coverImg } + ')',
+        backgroundPosition: 'center',
+        backgroundSize: 'cover',
+        backgroundAttachment: 'fixed'
+    };
+
     useEffect(() => {
         fetch('https://raw.githubusercontent.com/imprantu/revive-plus/main/doctors.json')
             .then(res => res.json())
@@ -20,31 +31,35 @@ const DoctorDetails = () => {
     }, [docDetails]);
 
     return (
-        <div className="container my-5">
-            <div className="doc-info-card mx-auto">
-                <div className="doctor-info">
-                    <img src={details?.imgWide} alt="" />
-                    <div className="text-center my-3">
-                        <h1 className="fw-bold text-primary">{details?.name}</h1>
-                        <p className="fs-3 fw-bold"><span className="text-secondary">{details?.role}</span> of {details?.medical}</p>
-                        <p className="fs-4 fw-bold">{details?.country}</p>
-                        <p className="text-muted">{details?.experience} Years of Experience</p>
-                    </div>
-                    <hr />
-                    <div>
-                        <p className="fs-4 fw-bold text-center text-info">Patient Check Up Time</p>
-                        <Row sm={2} md={2} lg={2} xl={2} className="text-center border border-1 my-2 g-3 work-time">
-                            <Col>
-                                <p>Monday-Friday</p>
-                                <p>Saturday</p>
-                                <p>Sunday</p>
-                            </Col>
-                            <Col>
-                                <p>08: 00 - 17: 00</p>
-                                <p>10: 00 - 16: 00</p>
-                                <p>12: 00 - 14: 00</p>
-                            </Col>
-                        </Row>
+        <div>
+            <div className="cover-img" style={{ backgroundImage: `url(${coverImg})`, backgroundPosition: 'center', backgroundRepeat: 'no-repeat', backgroundAttachment: 'fixed', backgroundSize: 'cover', height: '90vh' }}>
+                {/* background image goes here */}
+            </div>
+            <div className="container my-5">
+                <div className="doc-info-card mx-auto">
+                    <div className="doctor-info">
+                        <div className="text-center my-3">
+                            <h1 className="fw-bold text-primary">{details?.name}</h1>
+                            <p className="fs-3 fw-bold"><span className="text-secondary">{details?.role}</span> of {details?.medical}</p>
+                            <p className="fs-4 fw-bold">{details?.country}</p>
+                            <p className="text-muted">{details?.experience} Years of Experience</p>
+                        </div>
+                        <hr />
+                        <div>
+                            <p className="fs-4 fw-bold text-center text-info">Patient Check Up Time</p>
+                            <Row sm={2} md={2} lg={2} xl={2} className="text-center border border-1 my-2 g-3 work-time">
+                                <Col>
+                                    <p>Monday-Friday</p>
+                                    <p>Saturday</p>
+                                    <p>Sunday</p>
+                                </Col>
+                                <Col>
+                                    <p>08: 00 - 17: 00</p>
+                                    <p>10: 00 - 16: 00</p>
+                                    <p>12: 00 - 14: 00</p>
+                                </Col>
+                            </Row>
+                        </div>
                     </div>
                 </div>
             </div>
