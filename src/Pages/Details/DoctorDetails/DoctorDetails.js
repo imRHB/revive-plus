b0f7d9eee57bbc2d7cb55efe2000c912f105ff06
imprantu <rakinhasanbabu4@gmail.com>
2021-10-19 18:08:08 +0600

    doctors details added

--- src/Pages/Details/DoctorDetails/DoctorDetails.js
+++ src/Pages/Details/DoctorDetails/DoctorDetails.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
+import { Card, Row, Col } from 'react-bootstrap';
 import { useParams } from 'react-router';
+import './DoctorDetails.css';
 
 const DoctorDetails = () => {
     const { docId } = useParams();
@@ -15,14 +17,38 @@ const DoctorDetails = () => {
     useEffect(() => {
         const doctorInfo = doctorsDetails.find(docDetail => docDetail?.id == docId);
         setDocDetails(doctorInfo);
-        console.log(doctorInfo);
     }, [doctorsDetails]);
 
     return (
-        <div className="container my-5 py-4">
-            <h5>Doctor details of {docId}</h5>
-            <h3>{docDetails?.name}</h3>
-        </div>
+        <div className="container my-5">
+            <div className="doc-info-card mx-auto">
+                <div className="doctor-info">
+                    <img src={docDetails?.imgWide} alt="" />
+                    <div className="text-center my-3">
+                        <h1 className="fw-bold text-primary">{docDetails?.name}</h1>
+                        <p className="fs-3 fw-bold"><span className="text-secondary">{docDetails?.role}</span> of {docDetails?.medical}</p>
+                        <p className="fs-4 fw-bold">{docDetails?.country}</p>
+                        <p className="text-muted">{docDetails?.experience} Years of Experience</p>
+                    </div>
+                    <hr />
+                    <div>
+                        <p className="fs-4 fw-bold text-center text-info">Patient Check Up Time</p>
+                        <Row sm={2} md={2} lg={2} className="text-center border border-1 my-2 g-3 work-time">
+                            <Col>
+                                <p>Monday-Friday</p>
+                                <p>Saturday</p>
+                                <p>Sunday</p>
+                            </Col>
+                            <Col>
+                                <p>08: 00 - 17: 00</p>
+                                <p>10: 00 - 16: 00</p>
+                                <p>12: 00 - 14: 00</p>
+                            </Col>
+                        </Row>
+                    </div>
+                </div>
+            </div>
+        </div >
     );
 };
 
